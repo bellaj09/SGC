2d321ccc3b004020416a313b7f7563dc6df4f359
Isabella Juria <icjuria@gmail.com>
2020-12-18 08:32:07 +1100

    updating biobert finetuning

--- downstream/TextSGC_Bio/data/biobert_finetune.py
+++ downstream/TextSGC_Bio/data/biobert_finetune.py
@@ -50,26 +50,29 @@ train_dataset = OhsumedDataset(train_encodings, train_labels)
 val_dataset = OhsumedDataset(val_encodings, val_labels)
 test_dataset = OhsumedDataset(test_encodings, test_labels)
 
-from transformers import BertForSequenceClassification, Trainer, TrainingArguments
+from torch.utils.data import DataLoader
+from transformers import BertForSequenceClassification, AdamW
 
-training_args = TrainingArguments(
-    output_dir='./results',          # output directory
-    num_train_epochs=3,              # total number of training epochs
-    per_device_train_batch_size=16,  # batch size per device during training
-    per_device_eval_batch_size=64,   # batch size for evaluation
-    warmup_steps=500,                # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # strength of weight decay
-    logging_dir='./logs',            # directory for storing logs
-    logging_steps=10,
-)
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 model = BertForSequenceClassification.from_pretrained("dmis-lab/biobert-large-cased-v1.1",num_labels = 23)
+model.to(device)
+model.train()
 
-trainer = Trainer(
-    model=model,                         # the instantiated 🤗 Transformers model to be trained
-    args=training_args,                  # training arguments, defined above
-    train_dataset=train_dataset,         # training dataset
-    eval_dataset=val_dataset             # evaluation dataset
-)
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 
-trainer.train()
+optim = AdamW(model.parameters(), lr=5e-5)
+
+for epoch in range(3):
+    for batch in train_loader:
+        optim.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs[0]
+        loss.backward()
+        optim.step()
+
+model.eval()
+torch.save(model,'corpus/')
