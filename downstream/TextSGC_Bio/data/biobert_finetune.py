d84366bfd9e7603e4cffc830ebbda09123d26138
Isabella Juria <icjuria@gmail.com>
2020-12-19 18:50:46 +1000

    finetuning

--- downstream/TextSGC_Bio/data/biobert_finetune.py
+++ downstream/TextSGC_Bio/data/biobert_finetune.py
@@ -170,3 +170,23 @@ model = BertForSequenceClassification.from_pretrained(
 
 # Tell pytorch to run this model on the GPU.
 model.cuda()
+
+# Get all of the model's parameters as a list of tuples.
+params = list(model.named_parameters())
+
+print('The BERT model has {:} different named parameters.\n'.format(len(params)))
+
+print('==== Embedding Layer ====\n')
+
+for p in params[0:5]:
+    print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+
+print('\n==== First Transformer ====\n')
+
+for p in params[5:21]:
+    print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+
+print('\n==== Output Layer ====\n')
+
+for p in params[-4:]:
+    print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
