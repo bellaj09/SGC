0c9789b64dbfa3edb8e2a18d6704a87a02863858
Isabella Juria <icjuria@gmail.com>
2020-12-19 18:31:54 +1000

    biobert finetuning

--- downstream/TextSGC_Bio/data/biobert_finetune.py
+++ downstream/TextSGC_Bio/data/biobert_finetune.py
@@ -77,3 +77,42 @@ for sent in all_texts:
     max_len = max(max_len, len(input_ids))
 
 print('Max sentence length: ', max_len)
+
+max_len = 512 # setting to the max length.
+
+# Tokenize all of the sentences and map the tokens to their word IDs.
+input_ids = []
+attention_masks = []
+
+# For every sentence...
+for sent in all_texts:
+    # `encode_plus` will:
+    #   (1) Tokenize the sentence.
+    #   (2) Prepend the `[CLS]` token to the start.
+    #   (3) Append the `[SEP]` token to the end.
+    #   (4) Map tokens to their IDs.
+    #   (5) Pad or truncate the sentence to `max_length`
+    #   (6) Create attention masks for [PAD] tokens.
+    encoded_dict = tokenizer.encode_plus(
+                        sent,                      # Sentence to encode.
+                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                        max_length = max_len,           # Pad & truncate all sentences.
+                        pad_to_max_length = True,
+                        return_attention_mask = True,   # Construct attn. masks.
+                        return_tensors = 'pt',     # Return pytorch tensors.
+                   )
+    
+    # Add the encoded sentence to the list.    
+    input_ids.append(encoded_dict['input_ids'])
+    
+    # And its attention mask (simply differentiates padding from non-padding).
+    attention_masks.append(encoded_dict['attention_mask'])
+
+# Convert the lists into tensors.
+input_ids = torch.cat(input_ids, dim=0)
+attention_masks = torch.cat(attention_masks, dim=0)
+labels = torch.tensor(all_labels)
+
+# Print sentence 0, now as a list of IDs.
+print('Original: ', all_texts[0])
+print('Token IDs:', input_ids[0])
