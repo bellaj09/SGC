4c6c4012c62736d4c3352d98875e6d0a20c65d90
Isabella Juria <icjuria@gmail.com>
2020-12-19 18:43:06 +1000

    finetuning test

--- downstream/TextSGC_Bio/data/biobert_finetune.py
+++ downstream/TextSGC_Bio/data/biobert_finetune.py
@@ -13,7 +13,7 @@ import re
 from sklearn.model_selection import train_test_split
 import torch
 from torch.optim import Adam
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler, random_split
 from tqdm import tqdm, trange
 import os
 import math
@@ -100,6 +100,7 @@ for sent in all_texts:
                         pad_to_max_length = True,
                         return_attention_mask = True,   # Construct attn. masks.
                         return_tensors = 'pt',     # Return pytorch tensors.
+                        truncation = True
                    )
     
     # Add the encoded sentence to the list.    
@@ -116,3 +117,56 @@ labels = torch.tensor(all_labels)
 # Print sentence 0, now as a list of IDs.
 print('Original: ', all_texts[0])
 print('Token IDs:', input_ids[0])
+
+# Dividing dataset into train/test
+
+# Combine the training inputs into a TensorDataset.
+dataset = TensorDataset(input_ids, attention_masks, labels)
+
+# Create a 90-10 train-validation split.
+# Calculate the number of samples to include in each set.
+train_size = int(0.9 * len(dataset))
+val_size = len(dataset) - train_size
+
+# Divide the dataset by randomly selecting samples.
+train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+print('{:>5,} training samples'.format(train_size))
+print('{:>5,} validation samples'.format(val_size))
+
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+
+# The DataLoader needs to know our batch size for training, so we specify it 
+# here. For fine-tuning BERT on a specific task, the authors recommend a batch 
+# size of 16 or 32.
+batch_size = 32
+
+# Create the DataLoaders for our training and validation sets.
+# We'll take training samples in random order. 
+train_dataloader = DataLoader(
+            train_dataset,  # The training samples.
+            sampler = RandomSampler(train_dataset), # Select batches randomly
+            batch_size = batch_size # Trains with this batch size.
+        )
+
+# For validation the order doesn't matter, so we'll just read them sequentially.
+validation_dataloader = DataLoader(
+            val_dataset, # The validation samples.
+            sampler = SequentialSampler(val_dataset), # Pull out batches sequentially.
+            batch_size = batch_size # Evaluate with this batch size.
+        )
+
+from transformers import BertForSequenceClassification, AdamW, BertConfig
+
+# Load BertForSequenceClassification, the pretrained BERT model with a single 
+# linear classification layer on top. 
+model = BertForSequenceClassification.from_pretrained(
+    "dmis-lab/biobert-large-cased-v1.1", # Use the 12-layer BERT model, with an uncased vocab.
+    num_labels = 23, # The number of output labels--2 for binary classification.
+                    # You can increase this for multi-class tasks.   
+    output_attentions = False, # Whether the model returns attentions weights.
+    output_hidden_states = False, # Whether the model returns all hidden-states.
+)
+
+# Tell pytorch to run this model on the GPU.
+model.cuda()
