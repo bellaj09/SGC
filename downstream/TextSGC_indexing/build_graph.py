0a122b37d4f9dcec0ee1606aa3c89d5539713255
Isabella Juria <icjuria@gmail.com>
2020-12-22 00:08:18 +1000

    permutation importance

--- downstream/TextSGC_indexing/build_graph.py
+++ downstream/TextSGC_indexing/build_graph.py
@@ -125,10 +125,22 @@ dtf_features = pd.DataFrame()
 
 # # FEATURE GINI IMPORTANCES (DECISION TREES)
 from sklearn.tree import DecisionTreeClassifier
+# for cat in np.unique(y):
+#     tree = DecisionTreeClassifier().fit(X_train, y==cat)
+#     p = tree.feature_importances_
+#     print('min gini: ', np.min(p), 'max gini: ', np.max(p), 'mean gini: ', np.mean(p), 'median gini: ', np.median(p))
+#     dtf_features = dtf_features.append(pd.DataFrame(
+#                     {"feature":X_names, "score":p, "y":cat}))
+#     dtf_features = dtf_features.sort_values(["y","score"], 
+#                     ascending=[True,False])
+#     dtf_features = dtf_features[dtf_features["score"]>p_value_limit]
+
+## FEATURE PERMUTATION IMPORTANCES
+from sklearn.inspection import permutation_importance
 for cat in np.unique(y):
     tree = DecisionTreeClassifier().fit(X_train, y==cat)
-    p = tree.feature_importances_
-    print('min gini: ', np.min(p), 'max gini: ', np.max(p), 'mean gini: ', np.mean(p), 'median gini: ', np.median(p))
+    p = permutation_importance(tree, X_train,y==cat, n_repeats=10, random_state=42)
+    print('min perm: ', np.min(p), 'max perm: ', np.max(p), 'mean perm: ', np.mean(p), 'median perm: ', np.median(p))
     dtf_features = dtf_features.append(pd.DataFrame(
                     {"feature":X_names, "score":p, "y":cat}))
     dtf_features = dtf_features.sort_values(["y","score"], 
