e3ebcd39553da1756ee5df12a2685fe30b21e310
Isabella Juria <icjuria@gmail.com>
2020-12-24 22:34:38 +1000

    updating graph building

--- downstream/TextSGC_indexing/build_graph.py
+++ downstream/TextSGC_indexing/build_graph.py
@@ -385,8 +385,8 @@ def build_word_word_graph(num_window, word_id_map, word_window_freq, word_pair_c
             pmi = log((1.0 * count / num_window) /
                     (1.0 * word_freq_i * word_freq_j/(num_window * num_window)))
             
-            if pmi < 0: # only append weights if words frequently co-occur
-                pmi = 0
+            # if pmi < 0: # only append weights if words frequently co-occur
+            #     pmi = 0
 
             if i in word_vector_map and j in word_vector_map:
                 ### BIOBERT
@@ -403,12 +403,12 @@ def build_word_word_graph(num_window, word_id_map, word_window_freq, word_pair_c
                 # vector_j = np.array(word_vector_map[j])
     
                 similarity = 1.0 - cosine(vector_i, vector_j)
+                pmi = similarity + pmi
                 
-                
-                if similarity >= 0.3: # if very similar
-                    pmi = 2*similarity + pmi
-                else:
-                    pmi = similarity + pmi
+                # if similarity >= 0.3: # if very similar
+                #     pmi = 2*similarity + pmi
+                # else:
+                #     pmi = similarity + pmi
 
             row.append(word_id_map[i])
             col.append(word_id_map[j])
