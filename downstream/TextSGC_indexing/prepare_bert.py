4a6909dda4966b3c5b80c1067422957acc55690c
Isabella Juria <icjuria@gmail.com>
2020-12-24 17:53:07 +1000

    prepping for biobert training

---
+++ downstream/TextSGC_indexing/prepare_bert.py
@@ -0,0 +1,23 @@
+import argparse
+from nltk.tokenize import sent_tokenize
+import re
+
+parser = argparse.ArgumentParser(description='Prepare corpora for training BioBERT')
+parser.add_argument('--dataset', type=str, default='20ng',
+                    choices=['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'yelp', 'ag_news', 'covid_19_production','pubmed'],
+                    help='dataset name')
+
+doc_content_list = []
+f = open('../data/corpus/' + dataset + '.txt', 'rb')
+for line in f.readlines():
+    doc_content_list.append(line.strip().decode('latin1'))
+
+with open('data/corpus/'+ dataset + '_trainbiobert.txt', 'w') as f:
+    for doc in doc_content_list:
+        sent = sent_tokenize(doc)
+        for i,s in enumerate(sent):
+            s = s.strip().lower()
+            s = re.sub(r'[^a-zA-Z0-9  -]',r'',s)
+            sent[i] = s
+        f.write('\n'.join(sent)) # write each sentence on a new line
+        f.write('\n') # empty line at end of doc.
