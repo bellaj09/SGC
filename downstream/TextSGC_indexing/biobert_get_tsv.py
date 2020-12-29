e189ef9f4edb98e4c6295777007ce2590ea69082
Isabella Juria <icjuria@gmail.com>
2020-12-29 16:22:49 +1000

    standard biobert infect embeddings

--- downstream/TextSGC_indexing/biobert_get_tsv.py
+++ downstream/TextSGC_indexing/biobert_get_tsv.py
@@ -7,37 +7,37 @@ word_vector_map = h5py.File(embedding_path, 'r')
 
 ### Closest words to infect
 
-# closest_words = ['infect', 'infected', 'infection', 'infectious', 'virusinfect','hivinfect','covid19infect','coinfect','infectionrelated','influenza','infectivity']
-# corp_vocab = []
-# vectors = []
-
-# for word in closest_words:
-#     corp_vocab.append(str(word))
-#     vectors.append(np.array(word_vector_map[word]['embedding'][:]))
-
-# with open('data/biobert_infect_vocab.tsv', 'w', newline='') as f_output:
-#     tsv_output = csv.writer(f_output, delimiter='\n')
-#     tsv_output.writerow(corp_vocab)
-
-# with open('data/biobert_infect_vectors.tsv', 'w', newline='') as f_output:
-#     tsv_output = csv.writer(f_output, delimiter='\t',lineterminator='\n')
-#     for v in vectors:
-#         tsv_output.writerow(v)
-
-## all embeddings
-
+closest_words = ['infect', 'influent', 'infective', 'coinfect', 'infector','infecting','influenza','infected','infectivty','infectious','infection']
 corp_vocab = []
 vectors = []
 
-for word in word_vector_map:
+for word in closest_words:
     corp_vocab.append(str(word))
     vectors.append(np.array(word_vector_map[word]['embedding'][:]))
 
-with open('data/biobert_all_vocab.tsv', 'w', newline='') as f_output:
+with open('data/biobert_infect_vocab.tsv', 'w', newline='') as f_output:
     tsv_output = csv.writer(f_output, delimiter='\n')
     tsv_output.writerow(corp_vocab)
 
-with open('data/biobert_all_vectors.tsv', 'w', newline='') as f_output:
+with open('data/biobert_infect_vectors.tsv', 'w', newline='') as f_output:
     tsv_output = csv.writer(f_output, delimiter='\t',lineterminator='\n')
     for v in vectors:
         tsv_output.writerow(v)
+
+## all embeddings
+
+# corp_vocab = []
+# vectors = []
+
+# for word in word_vector_map:
+#     corp_vocab.append(str(word))
+#     vectors.append(np.array(word_vector_map[word]['embedding'][:]))
+
+# with open('data/biobert_all_vocab.tsv', 'w', newline='') as f_output:
+#     tsv_output = csv.writer(f_output, delimiter='\n')
+#     tsv_output.writerow(corp_vocab)
+
+# with open('data/biobert_all_vectors.tsv', 'w', newline='') as f_output:
+#     tsv_output = csv.writer(f_output, delimiter='\t',lineterminator='\n')
+#     for v in vectors:
+#         tsv_output.writerow(v)
