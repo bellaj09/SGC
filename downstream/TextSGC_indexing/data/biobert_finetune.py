3a3cfb0e7d540488bc1d98b88c630bf83a6b46bc
Isabella Juria <icjuria@gmail.com>
2020-12-30 13:27:41 +1000

    finetuning covid again

--- downstream/TextSGC_indexing/data/biobert_finetune.py
+++ downstream/TextSGC_indexing/data/biobert_finetune.py
@@ -58,7 +58,7 @@ else:
     device = torch.device("cpu")
 
 # Read Ohsumed dataset
-ohsumed_df = pd.read_csv('ohsumed0.txt', header=None, delimiter='\t')
+ohsumed_df = pd.read_csv('covid_19_production0.txt', header=None, delimiter='\t')
 
 label_names = set()
 
@@ -68,7 +68,7 @@ for i in ohsumed_df.index:
 
 # Get the texts from the corpus txt
 doc_content_list = []
-f = open('../../data/corpus/ohsumed.txt', 'rb')
+f = open('../../data/corpus/covid_19_production.txt', 'rb')
 for line in f.readlines():
     doc_content_list.append(line.strip().decode('latin1'))
 f.close()
@@ -154,7 +154,7 @@ for length in doc_lens:
 
 # Load the cleaned vocab -> tokens that are never split
 corp_vocab = []
-vocab_path = 'corpus/ohsumed.treebank.bio_vocab.txt'
+vocab_path = 'corpus/covid_19_production.treebank.bio_vocab.txt'
 with open(vocab_path,'r') as f:
     lines = f.readlines()
     for l in lines:
@@ -267,9 +267,9 @@ from transformers import BertForSequenceClassification, AdamW, BertConfig
 # Load BertForSequenceClassification, the pretrained BERT model with a single 
 # linear classification layer on top. 
 model = BertForSequenceClassification.from_pretrained(
-    '../../TextSGC_Bio/data/tuned_biobert_ohsumed', # use the already trained model, just further train now.
+    '../../TextSGC_Bio/data/tuned_biobert_covid', # use the already trained model, just further train now.
     #"dmis-lab/biobert-large-cased-v1.1", # Use the 12-layer BERT model, with an uncased vocab.
-    num_labels = 23, # The number of output labels--2 for binary classification.
+    num_labels = len(label_names), # The number of output labels--2 for binary classification.
                     # You can increase this for multi-class tasks.   
     output_attentions = False, # Whether the model returns attentions weights.
     output_hidden_states = False, # Whether the model returns all hidden-states.
@@ -570,7 +570,7 @@ df_stats
 
 # Saving best-practices: if you use defaults names for the model, you can reload it using from_pretrained()
 
-output_dir = './tuned_biobert_ohsumed/'
+output_dir = './tuned_biobert_covid/'
 
 # Create output directory if needed
 if not os.path.exists(output_dir):
